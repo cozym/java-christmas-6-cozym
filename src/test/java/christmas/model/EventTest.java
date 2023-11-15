0424232bb869b69a0bb93412be7204b966f5863d
cozym <ddongyee99@daum.net>
2023-11-15 23:57:00 +0900

    test: 특별할인 및 이벤트뱃지 테스트 추가

--- src/test/java/christmas/model/EventTest.java
+++ src/test/java/christmas/model/EventTest.java
@@ -29,4 +29,21 @@ class EventTest {
         event.getGiftMenu(120000);
         Assertions.assertEquals(event.giftDiscount(),25000);
     }
+
+    @DisplayName("이벤트 참여 불가능할 경우 이벤트 뱃지 반환 테스트")
+    @Test
+    void 이벤트뱃지_테스트() {
+        Order order = new Order("타파스-1");
+        event.applyDiscounts(3,order);
+        Assertions.assertEquals(event.getEventBadge(),"없음");
+    }
+
+    @DisplayName("특별할인 테스트")
+    @Test
+    void 특별할인_테스트() {
+        Assertions.assertEquals(event.specialDiscount(25),1000);
+        Assertions.assertEquals(event.specialDiscount(4),0);
+    }
+
+
 }
